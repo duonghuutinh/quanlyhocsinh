1ef16a82d7688f7e5deb630da86e189f574e9bf2
dht <duonghuutinh1408@gmail.com>
2024-11-15 00:33:02 +0700

    Update

--- partials/connectDB.php
+++ partials/connectDB.php
@@ -3,8 +3,8 @@
 // Thông tin kết nối
 $servername = "localhost"; // hoặc tên máy chủ của bạn
 $username = "root"; // tên người dùng mặc định của XAMPP
-$password = ""; // mật khẩu mặc định thường để trống
-$dbname = "quanlyhocsinh"; // tên cơ sở dữ liệu của bạn
+$password = "kunieucoi1994"; // mật khẩu mặc định thường để trống
+$dbname = "quanlyhocsinh1"; // tên cơ sở dữ liệu của bạn
 
 // Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,4 +13,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
-?>
+
